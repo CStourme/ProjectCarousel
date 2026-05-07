de6e2e64a0ad89b8a532239518af5788884f6210
ryoaspi <>
2026-05-07 14:10:51 +0200

    [Dev] Symbole detection

--- Assets/_/Code/Jürgen/Random.SpawnPoint.cs
+++ Assets/_/Code/Jürgen/Random.SpawnPoint.cs
@@ -1,24 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject itemPrefab;
     public Transform[] spawnPoints;
 
+    public Image m_image;
+
     void Awake()
     {
         SpawnItem();
     }
-
+    
     void SpawnItem()
     {
         if (spawnPoints.Length == 0) return;
         Debug.Log("TEST");
      
         int randomIndex = Random.Range(0, spawnPoints.Length);
-
-        itemPrefab.SetActive(true);
-        Instantiate(itemPrefab, spawnPoints[randomIndex].position, spawnPoints[randomIndex].rotation, spawnPoints[randomIndex]);
+        
+        _currentItem = Instantiate(itemPrefab, spawnPoints[randomIndex].position, spawnPoints[randomIndex].rotation, spawnPoints[randomIndex]);
 
     }
+
+    public bool IsItemActive()
+    {
+        if (!_currentItem) return false;
+
+        return _currentItem.activeSelf;
+    }
+    
+    private GameObject _currentItem;
 }
