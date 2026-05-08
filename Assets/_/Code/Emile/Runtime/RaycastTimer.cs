cc3ce1a88adfdfcbaaf6b5f5f6e132e609d55a7c
ryoaspi <>
2026-05-08 11:33:05 +0200

    [Dev] Correctif Scripts

--- Assets/_/Code/Emile/Runtime/RaycastTimer.cs
+++ Assets/_/Code/Emile/Runtime/RaycastTimer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,62 +6,83 @@ namespace FixPoint.Runtime
 {
     public class RaycastTimer : MonoBehaviour
     {
-        public System.Action OnScanComplete;
-        
-        [SerializeField] private LayerMask obstaclemask;
-        [SerializeField] private LayerMask layermask;
-        [SerializeField] private Slider scanSlider;
-        
-        private RaycastHit hitinfo;
+        #region Publics
 
-        private float timer;
-        private float maxTime = 1f;
-        
-        
-        void Start()
+        public Action<GameObject> OnScanComplete;
+
+        #endregion
+
+
+        #region Inspector
+
+        [SerializeField] private LayerMask obstacleMask;
+        [SerializeField] private LayerMask targetMask;
+        [SerializeField] private Slider scanSlider;
+
+        #endregion
+
+
+        #region Privates
+
+        private RaycastHit _hitInfo;
+
+        private float _timer;
+        private float _maxTime = 1f;
+
+        private bool _scanCompleted;
+
+        #endregion
+
+
+        #region Unity API
+
+        private void Start()
         {
             scanSlider.gameObject.SetActive(false);
+            scanSlider.maxValue = _maxTime;
         }
 
-        void Update()
+        private void Update()
         {
-            
             Vector3 direction = transform.forward;
-            
-            
-            if (Physics.Raycast(transform.position, direction, out hitinfo, 10f, obstaclemask)) return;
 
-            if (Physics.Raycast(transform.position, direction, out hitinfo, 10f, layermask))
+            // Bloque le scan si obstacle
+            if (Physics.Raycast(transform.position, direction, out _hitInfo, 10f, obstacleMask))
+            {
+                ResetScan();
+                return;
+            }
+
+            // Vérifie si on touche un objet du layer
+            if (Physics.Raycast(transform.position, direction, out _hitInfo, 10f, targetMask))
             {
                 scanSlider.gameObject.SetActive(true);
 
-                timer += Time.deltaTime;
+                _timer += Time.deltaTime;
 
-                scanSlider.value = timer;
+                scanSlider.value = _timer;
 
                 Debug.Log("Scan in progress");
 
-                if (timer >= maxTime)
+                // Déclenche UNE seule fois
+                if (_timer >= _maxTime && !_scanCompleted)
                 {
+                    _scanCompleted = true;
+
                     Debug.Log("Object scanned !");
-                    OnScanComplete?.Invoke();
+
+                    OnScanComplete?.Invoke(_hitInfo.collider.gameObject);
                 }
 
                 Debug.DrawRay(
                     transform.position,
-                    direction * hitinfo.distance,
+                    direction * _hitInfo.distance,
                     Color.red
                 );
             }
             else
             {
-                timer = 0;
-
-                scanSlider.value = 0;
-
-                scanSlider.gameObject.SetActive(false);
-
-                Debug.Log("No objects scanned");
+                ResetScan();
 
                 Debug.DrawRay(
                     transform.position,
@@ -68,7 +90,26 @@ namespace FixPoint.Runtime
                     Color.green
                 );
             }
-            
         }
+
+        #endregion
+
+
+        #region Main Methods
+
+        private void ResetScan()
+        {
+            _timer = 0;
+
+            _scanCompleted = false;
+
+            scanSlider.value = 0;
+
+            scanSlider.gameObject.SetActive(false);
+
+            Debug.Log("No objects scanned");
+        }
+
+        #endregion
     }
 }
